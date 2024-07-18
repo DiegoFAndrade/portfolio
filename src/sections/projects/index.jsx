03fb513e7954d4317e1c668682e5ab2dbb410481
diego-gbx <diego.fernandes@gbxbrasil.com>
2024-07-18 09:08:50 -0300

    fix: remoção de urls

--- src/sections/projects/index.jsx
+++ src/sections/projects/index.jsx
@@ -9,21 +9,21 @@ const portfolioData = [
     description:
       "O Ray Bank é um banco digital, que disponibiliza controle financeiro, pagamentos eletrônicos, gestão de recebimentos e suporte especializado, trazendo eficiência e comodidade para o negócio.",
     image: imgRayBank,
-    url: "https://raybank.authbank.com.br/",
+    // url: "https://raybank.authbank.com.br/",
   },
   {
     title: "Be as a Bank",
     description:
       "O Be as a Bank é uma aplicação de gerenciamento e suporte para soluções administrativas integradas à plataforma, disponíveis para facilitar o acompanhamento das transações, realizadas em tempo real.",
     image: imgBeasBank,
-    url: "https://backoffice-dev.beasabank.com.br/",
+    // url: "https://backoffice-dev.beasabank.com.br/",
   },
   {
     title: "Rastreamento veicular - Tracktus",
     description:
       "O rastreamento veicular é um serviço que permite monitorar a localização e o movimento de veículos por meio de dispositivos GPS instalados nos mesmos. ",
     image: imgTracktus,
-    url: "https://tech.tracktus.com.br/login",
+    // url: "https://tech.tracktus.com.br/login",
   },
 ];
 
@@ -39,17 +39,17 @@ export const Projects = () => {
         </h1>
         <div className="flex justify-between w-full flex-col lg:flex-row gap-8 md:gap-16 lg:gap-0">
           {portfolioData.map((project, index) => (
-            <a href={project.url} key={index} target="_blank">
-              <div className="flex flex-col text-white w-full lg:w-[280px] xl:w-[300px]  2xl:w-[340px] text-sm md:text-base gap-1.5 lg:gap-0">
-                <img
-                  alt={project.title}
-                  src={project.image}
-                  className="w-full h-auto rounded pb-1.5"
-                />
-                <p className="font-bold"> {project.title}</p>
-                <p className="text-gray-300 ">{project.description}</p>
-              </div>
-            </a>
+            // <a href={project.url} key={index} target="_blank">
+            <div className="flex flex-col text-white w-full lg:w-[280px] xl:w-[300px]  2xl:w-[340px] text-sm md:text-base gap-1.5 lg:gap-0">
+              <img
+                alt={project.title}
+                src={project.image}
+                className="w-full h-auto rounded pb-1.5"
+              />
+              <p className="font-bold"> {project.title}</p>
+              <p className="text-gray-300 ">{project.description}</p>
+            </div>
+            // </a>
           ))}
         </div>
       </div>
